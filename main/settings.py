66444711ab2f2cb71edc320c4906c003b1f827f7
zhongzhiqiang <200730249@qq.com>
2018-09-11 23:10:22 +0800

    fix:修改parseing

--- main/settings.py
+++ main/settings.py
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.FileUploadParser',
         'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.FileUploadParser',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',

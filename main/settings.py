4276f996df3dec438630de79932ee0935d7fcc2d
zhongzhiqiang <200730249@qq.com>
2018-09-11 22:43:14 +0800

    fix:修改文件上传解析

--- main/settings.py
+++ main/settings.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
         'rest_framework.parsers.FormParser',
+        'rest_framework.parser.FileUploadParser',
         'rest_framework.parsers.MultiPartParser',
     ),
     'DEFAULT_RENDERER_CLASSES': (

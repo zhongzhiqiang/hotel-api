a960abf6359e0837ad6d7204be0adefe4442fe0c
zhongzhiqiang <200730249@qq.com>
2018-10-17 20:29:15 +0800

    fix:修改问题

--- main/config/product.py
+++ main/config/product.py
@@ -5,3 +5,8 @@
 # File    : pro.py
 # Software: PyCharm
 from __future__ import unicode_literals
+REDIS_HOST = 'redis'
+REDIS_PORT = '6379'
+REDIS_PASSWORD = ''  # TODO
+ALLOWED_HOSTS = ['*']
+REDIS_HOST_PORT = '%s:%s' % (REDIS_HOST, REDIS_PORT)

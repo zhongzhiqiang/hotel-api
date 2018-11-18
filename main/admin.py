d026e1202ded3009de4f9bca9ea154ac1a3e485a
zhongzhiqiang <200730249@qq.com>
2018-11-18 21:31:54 +0800

    fix:修改admin的显示

--- main/admin.py
+++ main/admin.py
@@ -21,8 +21,16 @@ class OrderPayInline(admin.TabularInline):
     model = models.OrderPay
 
 
+class WxRefunedInfoInline(admin.TabularInline):
+    model = models.OrderRefunded
+
+
+class WxPayInfoInline(admin.TabularInline):
+    model = models.WeiXinPayInfo
+
+
 class OrderAdmin(admin.ModelAdmin):
-    inlines = [HotelOrderInline, MarketOrderContactInline, MarketOrderInline, OrderPayInline]
+    inlines = [HotelOrderInline, MarketOrderContactInline, MarketOrderInline, OrderPayInline, WxRefunedInfoInline, WxPayInfoInline]
 
     list_display = ['order_type', 'order_id', 'order_status', 'belong_hotel', 'pay_type', 'consumer']
 
@@ -38,10 +46,20 @@ class ConsumerBalanceAdmin(admin.ModelAdmin):
 class ConsumerAdmin(admin.ModelAdmin):
     list_display = ['id', 'user_name', 'phone', 'sex', 'bonus', 'recharge_balance']
 
+
+class WxPayInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'wx_order_id', 'create_time', 'call_back_result']
+
+
+class WxRefundedAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'refunded_status', 'refunded_order_id']
+
 admin.site.register(models.Order, OrderAdmin)
 admin.site.register(models.Goods, GoodsAdmin)
 admin.site.register(models.ConsumerBalance, ConsumerBalanceAdmin)
 admin.site.register(models.Consumer, ConsumerAdmin)
+admin.site.register(models.WeiXinPayInfo, WxPayInfoAdmin)
+admin.site.register(models.OrderRefunded, WxRefundedAdmin)
 for attr in dir(models):
     model = getattr(models, attr)
 
